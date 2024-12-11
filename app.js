2e9c198ad0b0918840cd41bd0f90828dfb3694c1
Morok1407 <bugaevdaniil82@gmail.com>
2024-12-11 08:19:53 +0500

    Добавил бота и обработку отправляемых с формы сообщений

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import mysql2 from "mysql2";
 import 'dotenv/config';
+import { Bot } from 'grammy'
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = 3000;
 
 function loadHost() {
@@ -42,10 +45,44 @@ app.get('/api/my_projects', (req, res) => {
     const sql = 'SELECT * FROM my_projects';
     db.query(sql, (err, result) => {
         if(err) {
-            concole.error(err);
-            res.staus(500).json({error: 'Ошибка получения данных'});
+            console.error(err);
+            res.status(500).json({error: 'Ошибка получения данных'});
             return;
         }
         res.json(result)
     })
 })
+
+const bot = new Bot(process.env.BOT_API);
+const targetUserId = 1203619482;
+function MyBot() {
+    bot.command('start', async (ctx) => {
+        if (ctx.from.id === targetUserId) {
+            ctx.reply('Привет хозяин!!!');
+        } else {
+            ctx.reply('Бот создан не для тебя!!! / The bot was not created for you!!!');
+        }
+    });
+
+    bot.start();
+}
+MyBot();
+
+app.post('/api/messageData', async (req, res) => {
+    const {name, email, text} = req.body;
+
+    const message = `
+    Новое сообщение с сайта:
+    \nИмя: ${name}
+    \nEmail: ${email}
+    \nСообщение: ${text}
+    `;
+
+    try {
+        res.status(200).json({ message: 'Данные успешно получены и отправлены в Telegram.' });
+        bot.api.sendMessage(targetUserId, message)
+    } catch (error) {
+        console.error('Ошибка при отправке в Telegram:', error);
+        res.status(500).json({ message: 'Ошибка при отправке сообщения в Telegram.' });
+    }
+});
